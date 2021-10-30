f5bcac1d7bb84478c3f49756098e840075fb4f25
Autumn Pham <autumnpham@Autumns-MacBook.local>
2021-10-29 19:37:02 -0700

    styling

--- src/components/PokeDetail.js
+++ src/components/PokeDetail.js
@@ -6,6 +6,7 @@ const PokeDetail = ({ pokemon, onReset }) => {
   if (pokemon === undefined) {
       return(
         <div className="card display-right">
+          <img src="https://assets.pokemon.com/assets/cms2/img/misc/countries/au/country_detail_pokemon.png" alt="pikachu" />
           <h5>Click on a Pokemon to view details!</h5>
         </div>
       );
@@ -14,7 +15,7 @@ const PokeDetail = ({ pokemon, onReset }) => {
         <div className="card display-right">
           <div className="flex-container">
             <h2 className="flex1">{pokemon.name}</h2>
-            <button className="btn-close flex2" onClick={onReset}></button>
+            <button className="btn-close-white flex2" onClick={onReset}>X</button>
           </div>
             <img src={pokemon.sprites.front_default} alt="pokemon img"/>
           <div className="pokedetails">
@@ -23,7 +24,7 @@ const PokeDetail = ({ pokemon, onReset }) => {
             <p>stats:</p>
             {pokemon.stats.map(({base_stat,stat:{name}})=>
             <ul>
-              <li>{name}:{base_stat}</li>
+              <li>{name}: {base_stat}</li>
             </ul>
             )}
           </div>
